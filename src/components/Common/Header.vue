e455abaa57e3e058c7d2489849ccd17033f632af
Arun Kumar <c2c.dba@gmail.com>
2025-01-26 13:24:09 -0500

    feat: Initial commit

---
+++ src/components/Common/Header.vue
@@ -0,0 +1,91 @@
+<template>
+    <header class="main-header">
+
+        <!--
+        <div class="topbar">
+            <div class="container">
+                <div class="topbar__left">
+                    <div class="topbar__social">
+                        <a href="#" class="fab fa-facebook-square"></a>
+                        <a href="#" class="fab fa-twitter"></a>
+                        <a href="#" class="fab fa-pinterest-p"></a>
+                        <a href="#" class="fab fa-instagram"></a>
+                    </div>
+                    <p>Welcome to Agriculture HTML Template</p>
+                </div
+                <div class="topbar__right">
+                    <a href="#"><i class="agrikon-icon-email"></i>needhelp@company.com</a>
+                    <a href="#"><i class="agrikon-icon-clock"></i>Mon - Sat 8:00 - 6:30, Sunday - CLOSED</a>
+                </div>
+            </div>
+        </div>
+      -->
+
+        <!-- /.topbar -->
+        <nav class="main-menu">
+            <div class="container">
+                <div class="logo-box">
+                    <a href="index.html" aria-label="logo image"><img src="assets/images/logo-dark.png" width="153"
+                            alt=""></a>
+                    <span class="fa fa-bars mobile-nav__toggler"></span>
+                </div><!-- /.logo-box -->
+                <ul class="main-menu__list">
+                    <li class="dropdown">
+                        <a href="index.html">Home</a>
+                        <ul>
+                            <li>
+                                <a href="#">Home One</a>
+                            </li>
+                            <li><a href="#">Home Two</a></li>
+                            <li class="dropdown">
+                                <a href="#">Header Styles</a>
+                                <ul>
+                                    <li><a href="#">Header One</a></li>
+                                    <li><a href="#">Header Two</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="#">About</a>
+                    </li>
+                    <li class="dropdown"><a href="#">Pages</a>
+                        <ul>
+                            <li><a href="#">Services</a></li>
+                            <li><a href="#">Service Details</a></li>
+                            <li><a href="#">Contact</a></li>
+                        </ul>
+                    </li>
+                    <li class="dropdown">
+                        <a href="projects.html">Projects</a>
+                        <ul>
+                            <li><a href="#">Projects</a></li>
+                            <li><a href="#">Projects Details</a></li>
+                        </ul>
+                    </li>
+                    <li class="dropdown"><a href="#">News</a>
+                        <ul>
+                            <li><a href="#">News</a></li>
+                            <li><a href="#">News Details</a></li>
+                        </ul>
+                    </li>
+                </ul>
+                <!-- /.main-menu__list -->
+
+                <div class="main-header__info">
+                    <a href="#" class="search-toggler main-header__search-btn"><i
+                            class="agrikon-icon-magnifying-glass"></i></a>
+                    <a class="main-header__cart-btn" href="#"><i class="agrikon-icon-shopping-cart"></i></a>
+                    <a href="tel:92-666-888-0000" class="main-header__info-phone">
+                        <i class="agrikon-icon-phone-call"></i>
+                        <span class="main-header__info-phone-content">
+                            <span class="main-header__info-phone-text">Call Anytime</span>
+                            <span class="main-header__info-phone-title">92 666 888 0000</span>
+                        </span><!-- /.main-header__info-phone-content -->
+                    </a><!-- /.main-header__info-phone -->
+                </div><!-- /.main-header__info -->
+            </div><!-- /.container -->
+        </nav>
+        <!-- /.main-menu -->
+    </header><!-- /.main-header -->
+</template>
